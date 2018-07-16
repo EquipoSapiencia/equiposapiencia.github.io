7d5958c29d14d93c544c760058f6ad0c55bedf02
Joar Esteban Buitrago Carrillo <jebuitragoc@unal.edu.co>
2018-07-16 16:26:50 -0500

    Creada cabecera con imagen de pantalla completa

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,12 +1,15 @@
 function openSidebar() {
     var icon = document.getElementById("menu");
     var sidebar = document.getElementById("sidebar");
+    var header = document.getElementById("header");
     var main = document.getElementById("main");
     var footer = document.getElementById("footer");
     
     if(icon.innerHTML === "menu") {
 	icon.innerHTML = "close";
 	sidebar.style.width = "200px";
+	header.style.marginRight = "200px";
+	header.style.marginLeft = "-200px";
 	main.style.marginRight = "200px";
 	main.style.marginLeft = "-200px";
 	footer.style.marginRight = "200px";
@@ -14,6 +17,8 @@ function openSidebar() {
     } else {
 	icon.innerHTML = "menu"
 	sidebar.style.width = "0";
+	header.style.marginRight = "0px";
+	header.style.marginLeft = "0px";
 	main.style.marginRight = "0";
 	main.style.marginLeft = "0";
 	footer.style.marginRight = "0px";
@@ -24,11 +29,14 @@ function openSidebar() {
 function closeSidebar() {
     var icon = document.getElementById("menu");
     var sidebar = document.getElementById("sidebar");
+    var header = document.getElementById("header");
     var main = document.getElementById("main");
     var footer = document.getElementById("footer");
     
     icon.innerHTML = "menu"
     sidebar.style.width = "0";
+    header.style.marginRight = "200px";
+    header.style.marginLeft = "-200px";
     main.style.marginRight = "0";
     main.style.marginLeft = "0";
     footer.style.marginRight = "0px";
