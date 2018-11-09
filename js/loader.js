898df30c7bacb43b53eeb308aee86bae9d4fab03
Joar Esteban Buitrago Carrillo <jebuitragoc@unal.edu.co>
2018-11-08 23:49:21 -0500

    Agregado cargador basico

---
+++ js/loader.js
@@ -0,0 +1,8 @@
+function loader() {
+    var tmp = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("page").style.display = "block";
+}
--- js/sidebar.js
+++ js/sidebar.js
@@ -6,23 +6,23 @@ function openSidebar() {
     var footer = document.getElementById("footer");
     
     if(icon.innerHTML === "menu") {
-	icon.innerHTML = "close";
-	sidebar.style.width = "200px";
-	header.style.marginRight = "200px";
-	header.style.marginLeft = "-200px";
-	main.style.marginRight = "200px";
-	main.style.marginLeft = "-200px";
-	footer.style.marginRight = "200px";
-	footer.style.marginLeft = "-200px";
+        icon.innerHTML = "close";
+        sidebar.style.width = "200px";
+        header.style.marginRight = "200px";
+        header.style.marginLeft = "-200px";
+        main.style.marginRight = "200px";
+        main.style.marginLeft = "-200px";
+        footer.style.marginRight = "200px";
+        footer.style.marginLeft = "-200px";
     } else {
-	icon.innerHTML = "menu"
-	sidebar.style.width = "0";
-	header.style.marginRight = "0px";
-	header.style.marginLeft = "0px";
-	main.style.marginRight = "0";
-	main.style.marginLeft = "0";
-	footer.style.marginRight = "0px";
-	footer.style.marginLeft = "0px";
+        icon.innerHTML = "menu";
+        sidebar.style.width = "0";
+        header.style.marginRight = "0px";
+        header.style.marginLeft = "0px";
+        main.style.marginRight = "0";
+        main.style.marginLeft = "0";
+        footer.style.marginRight = "0px";
+        footer.style.marginLeft = "0px";
     }
 }
 
@@ -33,7 +33,7 @@ function closeSidebar() {
     var main = document.getElementById("main");
     var footer = document.getElementById("footer");
     
-    icon.innerHTML = "menu"
+    icon.innerHTML = "menu";
     sidebar.style.width = "0";
     header.style.marginRight = "0px";
     header.style.marginLeft = "0px";
